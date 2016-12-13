a26f7fc603944bf5a2de05cf465216ace5dd97e6
Johan Bay <johanbay@gmail.com>
2016-12-13 17:54:19 +0100

    mc list

--- .mc-lists.el
+++ .mc-lists.el
@@ -4,18 +4,29 @@
 (setq mc/cmds-to-run-for-all
       '(
         TeX-insert-backslash
+        TeX-newline
         backward-sexp
         cdlatex-math-modify
+        cdlatex-pbb
+        cdlatex-sub-superscript
+        cdlatex-tab
         comment-dwim
+        company-coq-maybe-exit-snippet
+        coq-terminator-insert
         counsel-yank-pop
         easy-kill
         easy-mark
+        electric-newline-and-maybe-indent
         fill-paragraph
         forward-sexp
         indent-for-tab-command
         kill-region
+        message-beginning-of-line
+        message-tab
+        org-beginning-of-line
         org-delete-char
         org-self-insert-command
+        pg-protected-undo
         upcase-region
         ))
 
@@ -31,4 +42,5 @@
         hydra-multiple-cursors/mc/skip-to-previous-like-this
         hydra-multiple-cursors/mc/unmark-next-like-this
         hydra-multiple-cursors/mc/unmark-previous-like-this
+        ivy-switch-buffer
         ))
